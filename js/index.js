d69163ccd1c88770822fc3a63177e078589ae718
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-02-07 22:40:07 +0200

    added darkMood

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ const sidebarButton = document.querySelector(".sidebar__btn");
 sidebarButton.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+
+const changeMoodButton = document.querySelector(".changeMoodButton__icon");
+const features = document.querySelectorAll(".features .feature");
+const storyContent = document.querySelector(".story__content");
+const homes = document.querySelectorAll(".homes .home");
+const gallery = document.querySelector(".gallery");
+
+changeMoodButton.addEventListener("click", () => {
+  document.body.classList.toggle("darkMode");
+  changeMoodButton.classList.toggle("dark");
+  sidebar.classList.toggle("dark");
+  storyContent.classList.toggle("dark");
+  gallery.classList.toggle("dark");
+  features.forEach((feature) => {
+    feature.classList.toggle("dark");
+  });
+  homes.forEach((home) => {
+    home.classList.toggle("dark");
+  });
+});
